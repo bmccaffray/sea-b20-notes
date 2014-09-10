9bb42d27390e4badffd2209d3a5d2e5e951e0176
Brett McCaffray <Brett@bmp.local>
2014-09-10 11:32:39 -0700

    edit and add angular files instead of backbone, and finished crud structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,93 @@
 'use strict';
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
 
-  grunt.initConfig({
-    clean: {
-      dev: {
-        src: ['build/']
-      }
+    grunt.initConfig({
+        clean: {
+            dev: {
+                src: ['build/']
+        }
     },
 
     copy: {
-      dev: {
-        expand: true,
-        cwd: 'app/',
-        src: ['*.html', '*.css'],
-        dest: 'build/',
-        filter: 'isFile'
-      }
+        dev: {
+            expand: true,
+            cwd: 'app/',
+            src: ['*.html', '*.css', 'views/**/*.html'],
+            dest: 'build/',
+            filter: 'isFile'
+        }
     },
 
     browserify: {
-      dev: {
-        options: {
-          transform: ['debowerify', 'hbsfy'],
-          debug: true
+        dev: {
+            options: {
+                transform: ['debowerify'],
+                debug: true
+          },
+          src: ['app/js/**/*.js'],
+          dest: 'build/bundle.js'
         },
-        src: ['app/js/**/*.js'],
-        dest: 'build/bundle.js'
-      },
-      test: {
-        options: {
-          transform: ['hbsfy', 'debowerify'],
-          debug: true
-        },
-        src: ['test/mocha/backbone/**/*.js'],
-        dest: 'test/testbundle.js'
-      }
-    },
-
-    mocha: {
-      backbonetest: {
-        src: ['test/test.html'],
-        options: {
-          run: true
+        angulartest: {
+            options: {
+                transform: ['debowerify'],
+                debug: true
+            },
+            src: ['test/angular/**/*test.js'],
+            dest: 'test/angular-testbundle.js'
         }
-      }
     },
 
     simplemocha: {
-      all: {
-        src: ['test/mocha/api/**/*.js']
+        all: {
+            src: ['test/mocha/api/**/*.js']
+        }
+    },
+
+    karma: {
+        unit: {
+            configFile: 'karma.conf.js'
+        }
+    },
+
+    express: {
+        dev: {
+            options: {
+              options: 'server.js',
+              background: true
+          }
       }
+    },
+
+    watch: {
+        angulartest: {
+            files: ['app/js/**/*.js', 'app/index.html', 'app/views/**/*.html'],
+            tasks: ['browserify:angulartest', 'karma:unit'],
+            options: {
+                spawn: false
+            }
+        },
+        express: {
+            files: ['app/js/**/*.js', 'app/index.html', 'app/views/**/*.html', 'server.js', 'models/*.js', 'routes/*.js'],
+            tasks: ['buildtest', 'express:dev'],
+            options: {
+                pawn: false
+            }
+        }
     }
-  });
-  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
-  grunt.registerTask('backbone:test', ['browserify:test', 'mocha:backbonetest']);
-  grunt.registerTask('test', ['backbone:test', 'simplemocha']);
-  grunt.registerTask('default', ['test']);
+});
+
+grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
+grunt.registerTask('angulartest', ['browserify:angulartest', 'karma:unit']);
+grunt.registerTask('angulartestwatch', ['angulartest', 'watch:angulartest']);
+grunt.registerTask('test', ['angulartest', 'simplemocha']);
+grunt.registerTask('buildtest', ['test', 'build:dev']);
+grunt.registerTask('default', ['buildtest', 'watch:express']);
 };
